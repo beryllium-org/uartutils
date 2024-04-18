18f1eeb16ccb63707603f4a1626b5237139d5813
Bill Sideris <bill88t@feline.gr>
2024-04-19 00:07:52 +0300

    Fix uninstaller

--- files/uninstaller.py
+++ files/uninstaller.py
@@ -1,3 +1,3 @@
-be.based.rm("rm /bin/mkuart.lja /bin/mkuart.py /bin/rmuart.lja /bin/rmuart.py /bin/terminal.lja /bin/terminal.py /usr/share/help/mkuart.txt")
+be.based.run("rm /bin/mkuart.lja /bin/mkuart.py /bin/rmuart.lja /bin/rmuart.py /bin/terminal.lja /bin/terminal.py /usr/share/help/mkuart.txt")
 
 be.api.setvar("return", "0")
