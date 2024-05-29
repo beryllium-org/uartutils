b31a7404505e614944bb2d2dd3bdd544fc60859a
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:10:27 +0300

    copyfile

--- files/strap.py
+++ files/strap.py
@@ -6,6 +6,6 @@ for i in [
     "terminal.lja",
     "terminal.py",
 ]:
-    shutil.copy(i, path.join(root, "bin", i))
+    shutil.copyfile(i, path.join(root, "bin", i))
 
-shutil.copy("mkuart.txt", path.join(root, "usr/share/help", "mkuart.txt"))
+shutil.copyfile("mkuart.txt", path.join(root, "usr/share/help", "mkuart.txt"))
